636b2f72de36f47c0122d8d296d0c86c38d98552
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-18 17:35:23 -0500

    Setup task controllers

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import taskRoutes from './routes/tasks.js';
 
 const app = express();
+
+// middleware
+app.use(express.json());
 
 // routes
 app.get('/hello', (req, res) => {
 	res.send('Task Manager App');
 });
+
+app.use('/api/v1/tasks', taskRoutes);
 
 const port = 5000;
 
---
+++ controllers/tasks.js
@@ -0,0 +1,19 @@
+export const getAllTasks = (req, res) => {
+	res.send('all items from the file');
+};
+
+export const createTask = (req, res) => {
+	res.send('create task');
+};
+
+export const getTask = (req, res) => {
+	res.send('get single task');
+};
+
+export const updateTask = (req, res) => {
+	res.send('update task');
+};
+
+export const deleteTask = (req, res) => {
+	res.send('delete task');
+};
